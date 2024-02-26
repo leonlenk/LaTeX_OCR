c515be9b94a3433cb4d720764c2bc72f2429cd06
wz-ml <willzhou97@ucla.edu>
2024-02-25 16:53:28 -0800

    Added pretraining script on rendered LaTeX dataset.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ rm -r inkML_data/trainData_2012_part1/trainData_2012_part1
 rm -r inkML_data/trainData_2012_part2/trainData_2012_part2
 rm -r inkML_data/MatricesTrain2014/MatricesTrain
 rm -r inkML_data/TrainINKML_2013/TrainINKML
+wget https://zenodo.org/api/records/56198/files-archive
+unzip files-archive -d rendered_LaTeX
+tar -xvzf rendered_LaTeX/formula_images.tar.gz
 python run_preprocess.py
 rm archive.zip
